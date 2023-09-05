053d933d7dad6ab857df7422fa5d428761aa81c4
Barcallo <wtrelgar4869@gmail.com>
2023-09-05 16:07:38 +0900

    ガチャキャラ変更(0~5)
    ・ガチャのキャラクターの変更
    ・「引く！」文字を「1回召喚」に変更

---
+++ Assets/Scripts/Project2/P2_GachaManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class P2_GachaManager : MonoBehaviour
+{
+    [SerializeField]
+    int gachaPickCharacterId;
+
+    Perform_Gacha1 performGacha1;
+
+    void Awake()
+    {
+        gachaPickCharacterId = 0;
+        performGacha1 = new Perform_Gacha1();　//引いたキャラを表示するためのクラスをインスタンス。
+    }
+    void Start()
+    {
+        performGacha1.Init(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickGachaButton()
+    {
+        gachaPickCharacterId = Random.Range(0, 6); //0~5までのIDのキャラクターが出る。
+
+        Debug.Log(gachaPickCharacterId.ToString());
+
+        DrawPerform();
+
+        UserApplication.userDataManager.SetCharaHave(gachaPickCharacterId);
+        UserApplication.charaGridRenderer.RefreshGrid();
+    }
+
+    public void DrawPerform() //引いたキャラを表示する。
+    {
+        performGacha1.Activate();
+        FixCharaManager.FixCharaData fixCharaData = UserApplication.fixCharaManager.GetFixCharaData(gachaPickCharacterId);
+        performGacha1.SetSprite(Resources.Load<Sprite>(fixCharaData.imagePath));
+        performGacha1.SetName(fixCharaData.name);
+    }
+
+    public void OnClickGachaPerformBack() //キャラの表示をやめる。
+    {
+        performGacha1.Deactivate();
+    }
+}
