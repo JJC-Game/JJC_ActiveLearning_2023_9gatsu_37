fc342461a9252dc6950cc9831a7d23b3262766eb
Barcallo <wtrelgar4869@gmail.com>
2023-09-13 14:11:58 +0900

    DB連動
    ・DBと連動し、ゲーム終了後もキャラの所持状況が保存されるようになりました。

--- Etc/Laravel/AppUserController.php
+++ Etc/Laravel/AppUserController.php
@@ -13,7 +13,7 @@ class AppUserController extends Controller
         $has_chara_flag = $app_user->has_chara_flag;
         $result_string = "";
 
-        $gachaCount = 10;
+        $gachaCount = 1;
         for ($i = 0; $i < $gachaCount; $i++) {
             $rand_num = mt_rand(1, 10000);
             $jouyo = $rand_num % 32;
@@ -27,6 +27,27 @@ class AppUserController extends Controller
         return view('play_chara_gacha', ['result_string' => $result_string]);
 
     }
+
+    public function play_chara_gacha_10($id)
+    {
+        $app_user = AppUser::find($id);
+        $has_chara_flag = $app_user->has_chara_flag;
+        $result_string = "";
+
+        $gachaCount = 10;
+        for ($i = 0; $i < $gachaCount; $i++) {
+            $rand_num = mt_rand(1, 10000);
+            $jouyo = $rand_num % 32;
+            $result_string = $result_string.$jouyo.",";
+            $jouyo_flag = 1 << $jouyo;
+            $has_chara_flag = $has_chara_flag | $jouyo_flag;
+        }
+        $app_user->has_chara_flag = $has_chara_flag;
+        $app_user->save();
+
+        return view('play_chara_gacha', ['result_string' => $result_string]);
+    }
+
 
 
 
